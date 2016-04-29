16d70a94b028f82caf3c09c6c04017f05883678f
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-29 23:05:15 +0100

    Setup controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,50 +12,33 @@ class CommentsController < ApplicationController
   def show
   end
 
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
-
-  # GET /comments/1/edit
-  def edit
-  end
-
-  # POST /comments
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created }
       else
-        format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /comments/1
   # PATCH/PUT /comments/1.json
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
+        format.json { render :show, status: :ok }
       else
-        format.html { render :edit }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /comments/1
   # DELETE /comments/1.json
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
